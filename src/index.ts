0e88072fa6ac4a425193957e7741d61dd36535f6
AdityaP700 <126982848+AdityaP700@users.noreply.github.com>
2025-06-09 00:52:26 +0530

    Added the 4 basic Routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,45 @@
-import express, { Request, Response} from 'express'
+import express, { Request, Response} from 'express';
+import path from 'path';
+
 const app = express();
 const port = 3000;
 
-app.get('/',(req:Request, res:Response)=>{
-    res.send('Hello world!');
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to the ArcCoders ');
 });
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
+
+app.get('/api/user', (req: Request, res: Response) => {
+  res.json({
+    name:"Aditya NoobCoder",
+    age: 20,
+    city: "Bhubaneswar"
+  });
+});
+
+app.get('/hello', (req: Request, res: Response) => {
+  res.send('Hello WGMI!');
+});
+
+app.get('/html', (req: Request, res: Response) => {
+  res.send(`
+    <html>
+      <head><title>ArcCoders</title></head>
+      <body>
+        <h1>Hola WGMI</h1>
+        <p>YO a Preview!!.</p>
+      </body>
+    </html>
+  `);
+});
+
+app.get('/greet/:name', (req: Request, res: Response) => {
+  const { name } = req.params;
+  res.render('greeting', { name });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
